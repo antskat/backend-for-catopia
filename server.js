f85398f11fb65bc0e213985374ebb7f0b36d5f90
antskat <donaldsmak7@gmail.com>
2023-12-24 20:18:47 +0200

    add something

--- server.js
+++ server.js
@@ -8,15 +8,23 @@ import handleValidationErrors from "./server/utils/handleValidationErrors.js";
 import checkAuth from "./server/utils/checkAuth.js";
 import * as userController from "./server/controllers/userController.js";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { MONGO_URI, PORT } = process.env;
 
 const __dirname = path.resolve();
 
-const uri =
-  "mongodb+srv://admin:roottoor@catopia.0ss3vvb.mongodb.net/main?retryWrites=true&w=majority";
-
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose 
+  .connect(MONGO_URI)
   .then(() => {
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(`Server is running on port ${PORT}`);
+    });
     console.log("MongoDB connected");
   })
   .catch((err) => {
@@ -42,16 +50,8 @@ app.post(
   userController.register
 );
 
-app.get("/catopia/", (req, res) => {
-  res.redirect("http://antskat.github.io/catopia/");
-});
+app.get('/catopia/', (req, res) => {
+  res.redirect('http://antskat.github.io/catopia/');
+})
 
 app.get("/catopia/auth/me", checkAuth, userController.getMe);
-
-let port = 5000;
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(`Server is running on port ${port}`);
-});
